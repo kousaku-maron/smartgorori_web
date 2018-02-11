23fb51a6f08ebe2c974bf78e711c04e900daca8d
kousaku kurino <niconicoclimbing@gmail.com>
2018-02-11 12:45:14 +0900

    feat: notfoundpage width router

--- router.js
+++ router.js
@@ -10,29 +10,47 @@ module.exports = function(app, render) {
         })
 
         .get('/ccy', (ctx, next) => {
-            var json = JSON.parse(fs.readFileSync('views/assets/json/ccy.json', 'utf8')).articles;
+            var json = JSON.parse(fs.readFileSync('views/assets/json/ccys.json', 'utf8')).articles;
             var title = 'MARON crypto currency blog | crypto currency';
             var list_title = '仮想通貨一覧';
             return ctx.render('list_page', { title, list_title, json });
         })
 
         .get('/ccy/:id', (ctx, next) => {
-            var title = 'MARON crypto currency blog | crypto currency';
+            var json = JSON.parse(fs.readFileSync('views/assets/json/ccys.json', 'utf8')).articles;
             var id = ctx.params.id;
-            return ctx.render('article_page', { title, id });
+            
+            if(id <= json.length){
+                var title = 'MARON crypto currency blog | crypto currency';
+                var page = 'ccys/ccy' + id;
+                return ctx.render('article_page', { title, id, page });
+            }
+            else{
+                var title = 'MARON crypto currency blog | notfound';
+                return ctx.render('notfound', { title });
+            }
         })
         
         .get('/tech', (ctx, next) => {
-            var json = JSON.parse(fs.readFileSync('views/assets/json/tech.json', 'utf8')).articles;
+            var json = JSON.parse(fs.readFileSync('views/assets/json/techs.json', 'utf8')).articles;
             var title = 'MARON crypto currency blog | technology';
             var list_title = '技術一覧';
             return ctx.render('list_page', { title, list_title, json });
         })
         
         .get('/tech/:id', (ctx, next) => {
-            var title = 'MARON crypto currency blog | technology';
+            var json = JSON.parse(fs.readFileSync('views/assets/json/techs.json', 'utf8')).articles;
             var id = ctx.params.id;
-            return ctx.render('article_page', { title, id });
+
+            if(id <= json.length){
+                var title = 'MARON crypto currency blog | technology';
+                var page = 'techs/tech' + id;
+                return ctx.render('article_page', { title, id, page });
+            }
+            else{
+                var title = 'MARON crypto currency blog | notfound';
+                return ctx.render('notfound', { title });
+            }
         })
 
         .get('/article', (ctx, next) => {
@@ -43,9 +61,18 @@ module.exports = function(app, render) {
         })
 
         .get('/article/:id', (ctx, next) => {
-            var title = 'MARON crypto currency blog | article';
+            var json = JSON.parse(fs.readFileSync('views/assets/json/articles.json', 'utf8')).articles;
             var id = ctx.params.id;
-            return ctx.render('article_page', { title, id });
+            
+            if(id <= json.length){
+                var title = 'MARON crypto currency blog | article';
+                var page = 'articles/article' + id;
+                return ctx.render('article_page', { title, id, page });
+            }
+            else{
+                var title = 'MARON crypto currency blog | notfound';
+                return ctx.render('notfound', { title });
+            }
         })
 
         .get('*', (ctx, next) => {
