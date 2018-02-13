a250fc68bb372df8149e094a729aee683607c48d
kousaku kurino <niconicoclimbing@gmail.com>
2018-02-13 21:06:20 +0900

    fix: meta data

--- router.js
+++ router.js
@@ -2,21 +2,28 @@ const fs = require('fs');
 const Router = require('koa-router');
 const router = new Router();
 
+function notFound(){
+    var title = 'MARON crypto currency blog | Not Found';
+    var data = {title: 'Not Found', src: '/notfound'};
+    return ctx.render('notfound', { title, data });
+}
+
 module.exports = function(app, render) {
     router
         .get('/', (ctx, next) => {
             var json = JSON.parse(fs.readFileSync('views/assets/json/tiles.json', 'utf8')).tiles;
-            var title = 'MARON crypto currency blog | ホーム';
-            var data = {description: 'マロ〜ンの仮想通貨ブログです', image: '/images/icon.JPG', src: '/'};
+            var home = 'ホーム';
+            var title = 'MARON crypto currency blog | ' + home;
+            var data = {title: home, src: '/'};
             return ctx.render('home', { title, json, data });
         })
 
         .get('/ccy', (ctx, next) => {
             var json = JSON.parse(fs.readFileSync('views/assets/json/ccys.json', 'utf8')).articles;
-            var title = 'MARON crypto currency blog | 仮想通貨一覧';
             var list_title = '仮想通貨一覧';
-            var data = {description: 'マロ〜ンの仮想通貨ブログの仮想通貨一覧です', image: '/images/icon.JPG', src: '/ccy'};
-            return ctx.render('list_page', { title, list_title, json, data });
+            var title = 'MARON crypto currency blog | ' + list_title;
+            var data = {title: list_title, src: '/ccy'};
+            return ctx.render('list_page', { title, data, json });
         })
 
         .get('/ccy/:id', (ctx, next) => {
@@ -29,18 +36,16 @@ module.exports = function(app, render) {
                 return ctx.render('article_page', { title, data });
             }
             else{
-                var title = 'MARON crypto currency blog | notfound';
-                var data = {description: 'お探しのページはありません', image: '/images/notfound_ghost', 'src': '/notfound'};
-                return ctx.render('notfound', { title, data });
+                notFound();
             }
         })
         
         .get('/tech', (ctx, next) => {
             var json = JSON.parse(fs.readFileSync('views/assets/json/techs.json', 'utf8')).articles;
-            var title = 'MARON crypto currency blog | 技術一覧';
             var list_title = '技術一覧';
-            var data = {description: 'マロ〜ンの仮想通貨ブログの技術一覧です', image: '/images/icon.JPG', src: '/tech'};
-            return ctx.render('list_page', { title, list_title, json, data });
+            var title = 'MARON crypto currency blog | ' + list_title;
+            var data = {title: list_title, src: '/tech'};
+            return ctx.render('list_page', { title, data, json });
         })
         
         .get('/tech/:id', (ctx, next) => {
@@ -53,18 +58,16 @@ module.exports = function(app, render) {
                 return ctx.render('article_page', { title, data });
             }
             else{
-                var title = 'MARON crypto currency blog | notfound';
-                var data = {description: 'お探しのページはありません', image: '/images/notfound_ghost', src: '/notfound'};
-                return ctx.render('notfound', { title, data });
+                notFound();
             }
         })
 
         .get('/article', (ctx, next) => {
             var json = JSON.parse(fs.readFileSync('views/assets/json/articles.json', 'utf8')).articles;
-            var title = 'MARON crypto currency blog | 記事一覧';
             var list_title = '記事一覧';
-            var data = {description: 'マロ〜ンの仮想通貨ブログの記事一覧です', image: '/images/icon.JPG', src: '/article'};
-            return ctx.render('list_page', { title, list_title, json });
+            var title = 'MARON crypto currency blog | ' + list_title;
+            var data = {title: list_title, src: '/article'};
+            return ctx.render('list_page', { title, list_title, json, data });
         })
 
         .get('/article/:id', (ctx, next) => {
@@ -77,16 +80,12 @@ module.exports = function(app, render) {
                 return ctx.render('article_page', { title, data });
             }
             else{
-                var title = 'MARON crypto currency blog | notfound';
-                var data = {description: 'お探しのページはありません', image: '/images/notfound_ghost', src: '/notfound'};
-                return ctx.render('notfound', { title, data });
+                notFound();
             }
         })
 
         .get('*', (ctx, next) => {
-            var title = 'MARON crypto currency blog | notfound';
-            var data = {description: 'お探しのページはありません', image: '/images/notfound_ghost', src: '/notfound'};
-            return ctx.render('notfound', { title, data });
+            notFound();
         });
 
     return router.routes();
